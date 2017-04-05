59ec263ce86a9875dd53560c365aeba20069053a
Iacopo Poli <iacopo.poli93@gmail.com>
2017-04-05 14:35:40 +0200

    fixed cuda install

--- installing-cuda.sh
+++ installing-cuda.sh
@@ -6,6 +6,6 @@ sudo apt install gcc
 sudo apt-get install linux-headers-$(uname -r)
 
 # now install the package
-sudo dpkg -i cuda-repo-ubuntu1604-8-0-local-ga2_8.0.61-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
 sudo apt-get update
 sudo apt-get install cuda
